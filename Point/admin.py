03bf543aca18b355c11570a518c7fce710dda124
RahulRaj <rajr97333@gmail.com>
2020-03-13 17:08:01 +0100

    something

--- Point/admin.py
+++ Point/admin.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from import_export.admin import ImportExportModelAdmin
 import csv
 from .models import AdminUser, infection
+
 from django.conf import settings
 
 # Register your models here.
@@ -29,7 +30,37 @@ class ExportCsvMixin:
     export_as_csv.short_description = "Export Selected"
 
 
-class VenueAdmin(admin.ModelAdmin):
+# class CsvImportForm(forms.Form):
+#     csv_file = forms.FileField()
+
+
+# class HeroAdmin(admin.ModelAdmin, ExportCsvMixin):
+#     ...
+#     change_list_template = "entities/heroes_changelist.html"
+
+#     def get_urls(self):
+#         urls = super().get_urls()
+#         my_urls = [
+#             ...
+#             path('import-csv/', self.import_csv),
+#         ]
+#         return my_urls + urls
+
+#     def import_csv(self, request):
+#         if request.method == "POST":
+#             csv_file = request.FILES["csv_file"]
+#             reader = csv.reader(csv_file)
+#             # Create Hero objects from passed in data
+#             # ...
+#             self.message_user(request, "Your csv file has been imported")
+#             return redirect("..")
+#         form = CsvImportForm()
+#         payload = {"form": form}
+#         return render(
+#             request, "admin/csv_form.html", payload
+#         )
+
+class VenueAdmin(ImportExportModelAdmin, ExportCsvMixin):
     list_display = ["pk", 'email', 'user', 'location', 'frequency', 'latitude',
                     'longitude']
     search_fields = ('location',)
@@ -43,20 +74,20 @@ class VenueAdmin(admin.ModelAdmin):
 
     actions = ["export_as_csv"]
 
-    class Media:
-        if hasattr(settings, 'GOOGLE_MAPS_API_KEY') and settings.GOOGLE_MAPS_API_KEY:
-            css = {
-                'all': ('css/admin/location_picker.css',),
-            }
-            js = (
-                'https://maps.googleapis.com/maps/api/js?key={}'.format(
-                    settings.GOOGLE_MAPS_API_KEY),
-                'js/admin/location_picker.js',
-            )
+    # class Media:
+    #     if hasattr(settings, 'GOOGLE_MAPS_API_KEY') and settings.GOOGLE_MAPS_API_KEY:
+    #         css = {
+    #             'all': ('css/admin/location_picker.css',),
+    #         }
+    #         js = (
+    #             'https://maps.googleapis.com/maps/api/js?key={}'.format(
+    #                 settings.GOOGLE_MAPS_API_KEY),
+    #             'js/admin/location_picker.js',
+    #         )
 
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ["pk", 'start_hour', 'end_hour','location', 'address', 'date', 'place_type', 'latitude',
+class ProfileAdmin(ImportExportModelAdmin, ExportCsvMixin):
+    list_display = ["pk", 'start_hour', 'end_hour', 'location', 'address', 'date', 'place_type', 'latitude',
                     'longitude']
     search_fields = ('location',)
 
